87b3c094cf7a1f23daabcf756a3e0a34ed0d0bf8
muhammadhashir786 <muhammadhashiransari12@gmail.com>
2026-01-09 09:34:57 +0500

    Add 2 files from CWH folder

---
+++ SQL - Code With Harry/My Practise Script - 1 --- Create Database & Table.sql
@@ -0,0 +1,21 @@
+-- database named "startersql_1" created  
+-- table named "users_1" created
+
+-- CREATE DATABASE startersql_1;
+USE startersql_1;
+
+-- CREATE TABLE users_1 (
+-- 	id INT AUTO_INCREMENT PRIMARY KEY,
+--     name VARCHAR(100) NOT NULL,
+--     email VARCHAR(100) UNIQUE NOT NULL, 
+--     gender ENUM('Male', 'Female', 'Other'),
+--     date_of_birth DATE,
+--     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+-- );
+
+-- SELECT * FROM users_1;
+-- SELECT id, email FROM users_1;
+
+-- desc TABLE users_1;
+
+
---
+++ SQL - Code With Harry/My Practise Script - 2 --- Delete Table & Database.sql
@@ -0,0 +1,20 @@
+-- database named "startersql_2" created and deleted 
+-- table named "users_2" created and deleted 
+
+-- CREATE DATABASE startersql_2;
+USE startersql_2;
+
+-- CREATE TABLE users_2 (
+-- 	id INT AUTO_INCREMENT PRIMARY KEY,
+--     name VARCHAR(100) NOT NULL,
+--     email VARCHAR(100) UNIQUE NOT NULL, 
+--     gender ENUM('Male', 'Female', 'Other'),
+--     date_of_birth DATE,
+--     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+-- );
+
+-- DROP TABLE users_2;
+
+-- SELECT * FROM users_2;
+
+-- DROP DATABASE startersql_2;
